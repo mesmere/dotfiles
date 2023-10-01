9421afa2fd8eddfe40bfd72103df9d5593b728b4
Rileyann <95945959+mesmere@users.noreply.github.com>
2023-10-01 13:02:02 -0400

    Added doom sync to install.sh

--- install.sh
+++ install.sh
@@ -70,5 +70,8 @@ echo 'Stowing configs...'
 echo 'If this fails, you can run ./stow.sh manually as many times as you need to fix the problem.'
 source ./stow.sh
 
+echo 'Doom sync now that configs have been stowed...'
+$HOME/.config/emacs/bin/doom sync
+
 echo 'Done.'
 echo 'Do a full restart now before launching anything.'
