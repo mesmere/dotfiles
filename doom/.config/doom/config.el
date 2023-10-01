c91acb2cc09d8c2b7e515ca13a8cc0877f13efec
Rileyann <95945959+mesmere@users.noreply.github.com>
2023-10-01 16:01:43 -0400

    Reduce keyboard delay timeouts

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -75,5 +75,5 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(add-hook 'window-setup-hook #'doom/quickload-session)
 (setq kill-whole-line t)
+(setq which-key-idle-delay 0.15)
