cc482d2ec9f8716170318ea4f10e2cf0c60bd9dd
mesmere <95945959+mesmere@users.noreply.github.com>
2024-03-21 23:13:16 -0400

    Removed diffview
    I think doing this in-editor is overrated, lazygit works fine...

--- neovim/.config/nvim/lua/plugins/diffview.lua
+++
@@ -1,7 +0,0 @@
-return {{
-  "sindrets/diffview.nvim",
-  keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview pending changes" },
-    { "<leader>gD", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview current file history" },
-  }
-}}
