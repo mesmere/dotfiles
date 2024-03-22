51cfedeffb83cae281c89637ab86624124d43f64
mesmere <95945959+mesmere@users.noreply.github.com>
2024-03-21 22:25:39 -0400

    added diffview
    also set bufferline to always show bc diffview opens a new tab and
    bufferline doesn't open automatically in that case for some reason

---
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,6 @@
+return {{
+  "akinsho/bufferline.nvim",
+  opts = function(_, opts)
+    opts.options.always_show_bufferline = true
+  end,
+}}
---
+++ neovim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {{
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview pending changes" },
+    { "<leader>gD", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview current file history" },
+  }
+}}
